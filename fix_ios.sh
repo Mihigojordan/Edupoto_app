37ad4d942c2367e1b8462972b01338d52af64bc1
bukombe <jeanpaulbukombe@gmail.com>
2025-09-03 13:27:40 +0200

    Pod file updated and fixos.sh,updates Prj files

--- fix_ios.sh
+++ fix_ios.sh
@@ -3,24 +3,24 @@ set -e
 
 echo "🔧 Forcing iOS deployment target to 14.0..."
 
-# 1. Update Podfile
+# Podfile
 sed -i.bak "s/platform :ios, '.*'/platform :ios, '14.0'/" ios/Podfile || true
 
-# 2. Update AppFrameworkInfo.plist
+# AppFrameworkInfo.plist
 plutil -replace MinimumOSVersion -string "14.0" ios/Flutter/AppFrameworkInfo.plist
 
-# 3. Update project.pbxproj
+# project.pbxproj
 sed -i.bak "s/IPHONEOS_DEPLOYMENT_TARGET = [0-9.]\+/IPHONEOS_DEPLOYMENT_TARGET = 14.0/g" ios/Runner.xcodeproj/project.pbxproj || true
 
-# Run Flutter clean & pub get (to ensure fresh build)
+# Flutter clean & pub get
 flutter clean
 flutter pub get
 
-# Install CocoaPods
+# CocoaPods install
 cd ios
 pod repo update
 pod install
 cd ..
 
-# Finally build iOS without codesign
+# Build iOS
 flutter build ios --release --no-codesign
